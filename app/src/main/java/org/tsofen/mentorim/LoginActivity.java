85dcf006791e94b602d93488615cefae4165add8
Unknown <ayman.f.1993@gmail.com>
2017-09-17 15:08:38 +0300

    Team B
    Added colors
    Added login layout
    Added Login Activity

---
+++ app/src/main/java/org/tsofen/mentorim/LoginActivity.java
@@ -0,0 +1,15 @@
+package org.tsofen.mentorim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().setTitle(getString(R.string.signin));
+    }
+
+}
--- app/src/main/java/org/tsofen/mentorim/MainActivity.java
+++ app/src/main/java/org/tsofen/mentorim/MainActivity.java
@@ -1,5 +1,6 @@
 package org.tsofen.mentorim;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        login();
+        startActivity(new Intent(MainActivity.this,LoginActivity.class));
     }
 
     // Use-case example:
