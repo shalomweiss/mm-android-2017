81079a273f5e86820e916718020b683367203a93
Tony Zaitoun <tony.z.1711@gmail.com>
2017-09-19 13:06:13 +0300

    Modified Profile View Styles

--- app/src/main/java/org/tsofen/mentorim/MainActivity.java
+++ app/src/main/java/org/tsofen/mentorim/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profile);
 
         //create data manager instance
         final DataManager manager = DataManager.getInstance(this);
