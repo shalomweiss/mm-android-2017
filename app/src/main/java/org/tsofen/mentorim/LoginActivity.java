b4047ecdfea4c8b73f7f7c60ac8ef7665ec0d6ad
Unknown <ayman.f.1993@gmail.com>
2017-09-27 12:02:28 +0300

    sync server
    Team B

--- app/src/main/java/org/tsofen/mentorim/LoginActivity.java
+++ app/src/main/java/org/tsofen/mentorim/LoginActivity.java
@@ -2,6 +2,7 @@ package org.tsofen.mentorim;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
 
 import org.tsofen.model.APIManager;
@@ -48,7 +49,10 @@ public class LoginActivity extends AppCompatActivity {
 
         //Make API Call
         APIManager.getInstance().login(email, password,deviceId, (response, user, token, ex) -> {
+
+
             if (ex == null){
+                Log.i("Login", response.getMessage());
                 //create data manager instance from context
                 DataManager manager = DataManager.getInstance(this);
 
